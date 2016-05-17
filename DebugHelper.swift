60df0e2ce170e5aa5a4f6c324752b37830e558d1
Kazuaki Hara <bizhara@gmail.com>
2016-05-17 14:01:53 +0900

    Feedback

--- DebugHelper.swift
+++ DebugHelper.swift
@@ -16,17 +16,22 @@ extension NSObject {
   }
 }
 
-#if DEBUG
-  func dLog(log log_: AnyObject) {
-    print("\(log_)")
-  }
-  
-  func dLog(className className_: String, function function_: String = #function, line line_: Int = #line) {
-    print(">> \(className_).\(function_):\(line_)")
-  }
-#else
-  func dLog(log log_: AnyObject) {
-  }
-  func dLog(className className_: String, function function_: String = #function, line line_: Int = #line) {
-  }
-#endif
+// "#if DEBUG" を有効にするには、Swift Compiler - Custom Flags 中の Other Swift Flags の Debug 欄に "-DDEBUG" を加える
+
+@inline(__always) func dLogOnly(log log_: AnyObject) {
+  #if DEBUG
+    print(">> \(log_)")
+  #endif
+}
+
+@inline(__always) func dLog(filePath filePath_: String = #file, function function_: String = #function, line line_: Int = #line, log log_: AnyObject?) {
+  #if DEBUG
+    let paths = filePath_.componentsSeparatedByString("/")
+    let fileName = paths[paths.count - 1]
+    let className = fileName.componentsSeparatedByString(".")[0]
+    print(">> \(className).\(function_):\(line_)")
+    if let debugLog = log_ {
+      dLogOnly(log: debugLog)
+    }
+  #endif
+}
