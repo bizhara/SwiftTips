8dc45c26f2518505a7a662755939adeba9ef7808
Kazuaki Hara <bizhara@gmail.com>
2016-07-22 16:50:37 +0900

    Navigation bar 自身に透過を設定するメソッド追加
    appearance で全体的に設定するのは扱いが難しい場合が多い

--- UINavigationBar.extension.swift
+++ UINavigationBar.extension.swift
@@ -8,11 +8,32 @@ import UIKit
 
 extension UINavigationBar {
   /// 透過・透明なバー
+  func transparentBar() {
+    self.translucentBarWith(color: UIColor(red: 0, green: 0, blue: 0, alpha: 0))
+  }
+  
+  /// 透過色のバー（アプリ全ての UINavigationBar に影響）
   class func transparentBar() {
     self.translucentBarWith(color: UIColor(red: 0, green: 0, blue: 0, alpha: 0))
   }
   
   /// 透過色のバー
+  func translucentBarWith(color color_: UIColor) {
+    #if swift(>=3.0)
+    self.setBackgroundImage(UIImage(), for: .default)
+    #else
+    self.setBackgroundImage(UIImage(), forBarMetrics: .Default)
+    #endif
+    self.shadowImage = UIImage()
+    self.backgroundColor = color_
+    #if swift(>=3.0)
+    self.isTranslucent = true
+    #else
+    self.translucent = true
+    #endif
+  }
+  
+  /// 透過色のバー（アプリ全ての UINavigationBar に影響）
   class func translucentBarWith(color color_: UIColor) {
     #if swift(>=3.0)
     UINavigationBar.appearance().setBackgroundImage(UIImage(), for: .default)
