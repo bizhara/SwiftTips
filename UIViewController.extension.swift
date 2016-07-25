4c910f897d6ebce3034ab9eda4b3474b7284f36e
Kazuaki Hara <bizhara@gmail.com>
2016-07-25 18:28:09 +0900

    Use Set<NSObject> instead of NSSet after swift 1.2.

--- UIViewController.extension.swift
+++ UIViewController.extension.swift
@@ -8,21 +8,14 @@ import UIKit
 
 extension UIViewController {
   /// タッチした場所の UIView を返す (may use with touchesEnded)
-  #if swift(>=3.0)
   func findTouchedViewWith(touches touches_: Set<UITouch>, event event_: UIEvent?) -> UIView? {
-    let touch = touches_.first
-    let touchPoint = touch!.location(in: self.view)
+    guard let touch = touches_.first else {
+      return nil
+    }
+    let touchPoint = touch.location(in: self.view)
     let touchedView = self.view.hitTest(touchPoint, with: event_)
     return touchedView
   }
-  #else
-  func findTouchedViewWith(touches touches_: NSSet, event event_: UIEvent?) -> UIView? {
-  let touch = touches_.anyObject() as! UITouch
-  let touchPoint = touch.location(in: self.view)
-  let touchedView = self.view.hitTest(touchPoint, with: event_)
-  return touchedView
-  }
-  #endif
 
   // 遷移先の戻るボタンのタイトルを消すには、遷移元でこれを呼んでおく必要がある
   // ※ UINavigationControllerDelegate.navigationController(_, willShow, animated) で以下と同様のことを実装した方がスマートと思う
@@ -47,7 +40,7 @@ extension UIViewController {
 protocol ChildViewControllerProtocol: NSObjectProtocol {
   // 新たに画面上に表示した時や位置・サイズの変更があった時などに呼び出されることを想定している
   func didLayoutChildViewController(childViewController childViewController_: UIViewController)
-
+  
   // 使用が終わり画面上から消す時に呼ばれることを想定している
   func willHideChildViewController(childViewController childViewController_: UIViewController)
 }
