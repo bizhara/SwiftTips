436b202c3c85894dd4838a1ec967e673bf4fcef1
Kazuaki Hara <bizhara@gmail.com>
2016-07-25 16:55:35 +0900

    Add Swift 3.0 way.

--- UIResponder.extension.swift
+++ UIResponder.extension.swift
@@ -10,10 +10,18 @@ extension UIResponder {
   func hideKeyboard() {
     // "If target is nil, the app sends the message to the first responder" という API 仕様を利用して、
     // first responder に resignFirstResponder を送る
+    #if swift(>=3.0)
+    UIApplication.shared().sendAction(#selector(self.resignFirstResponder), to: nil, from: nil, for: nil)
+    #else
     UIApplication.sharedApplication().sendAction(#selector(self.resignFirstResponder), to: nil, from: nil, forEvent: nil)
+    #endif
   }
-  
+
   class func hideKeyboard() {
+    #if swift(>=3.0)
+    UIApplication.shared().hideKeyboard()
+    #else
     UIApplication.sharedApplication().hideKeyboard()
+    #endif
   }
 }
