c69948769bd3e22512cb783ec458709a24ed529a
Kazuaki Hara <bizhara@gmail.com>
2016-08-05 13:25:49 +0900

    Add verticalPercentAlignmentValueWith to playground.

--- SwiftSingletonWithParameter.playground/Contents.swift
+++ SwiftSingletonWithParameter.playground/Contents.swift
@@ -2,6 +2,21 @@
 
 import UIKit
 
+func verticalPercentAlignmentValueWith(superViewFrame superViewFrame_: CGRect, targetViewFrame targetViewFrame_: CGRect) -> CGFloat {
+  let baseHeight = superViewFrame_.size.height / 2
+  let targetHeight = targetViewFrame_.size.height / 2
+  let heightGap = baseHeight - targetHeight
+  let yGap = heightGap - targetViewFrame_.origin.y
+  let targetValue = baseHeight - yGap
+  let percentValue = targetValue / baseHeight
+  return percentValue
+}
+
+let superViewFrame = CGRect(x: 0, y: 0, width: 375, height: 667)
+let targetViewFrame = CGRect(x: 148, y: 561, width: 79, height: 46)
+let percentValue = verticalPercentAlignmentValueWith(superViewFrame: superViewFrame, targetViewFrame: targetViewFrame)
+
+
 class SwiftSingletonWithParameters: NSObject {
     private static let myInstance = SwiftSingletonWithParameters()
     
