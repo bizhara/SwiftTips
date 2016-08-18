ee00fcbbd0fa19456c335c3c7488ab6c3b475489
Kazuaki Hara <bizhara@gmail.com>
2016-08-18 22:03:38 +0900

    Use swift native type.

--- DebugHelper.swift
+++ DebugHelper.swift
@@ -8,13 +8,13 @@ import Foundation
 
 // "#if DEBUG" を有効にするには、Swift Compiler - Custom Flags 中の Other Swift Flags の Debug 欄に "-DDEBUG" を加える
 
-@inline(__always) func dLogOnly(log log_: AnyObject) {
+@inline(__always) func dLogOnly(log log_: Any) {
 #if DEBUG
   print(">> \(log_)")
 #endif
 }
 
-@inline(__always) func dLog(filePath filePath_: String = #file, function function_: String = #function, line line_: Int = #line, log log_: AnyObject?) {
+@inline(__always) func dLog(filePath filePath_: String = #file, function function_: String = #function, line line_: Int = #line, log log_: Any?) {
 #if DEBUG
   let paths = filePath_.components(separatedBy: "/")
   let fileName = paths[paths.count - 1]
