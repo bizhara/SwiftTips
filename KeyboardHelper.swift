d519ba77d77923e0975afc8d6ef1c0768df7c67d
Kazuaki Hara <bizhara@gmail.com>
2016-08-24 14:52:16 +0900

    Extend base class for keyboard.

--- KeyboardHelper.swift
+++ KeyboardHelper.swift
@@ -10,10 +10,20 @@ import UIKit
 
 /// キーボード操作のある Base ViewController
 class ViewControllerWithKeyboard: UIViewController {
+  /// 入力エリア以外をタッチしたらキーボードを消す
   override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
     super.touchesEnded(touches, with: event)
 
     self.hideKeyboardWith(touches: touches, event: event)
+  }
+
+  func hideKeyboardWith(touches touches_: Set<UITouch>, event event_: UIEvent?) {
+    guard let toucedView = self.findTouchedViewWith(touches: touches_, event: event_) else {
+      return
+    }
+    if toucedView != self.inputView {
+      self.hideKeyboard()
+    }
   }
 }
 
@@ -29,28 +39,21 @@ extension UIResponder {
   }
 }
 
-extension UIViewController {
-  /// 入力エリア以外をタッチしたらキーボードを消す (may use with touchesEnded)
-  func hideKeyboardWith(touches touches_: Set<UITouch>, event event_: UIEvent?) {
-    guard let toucedView = self.findTouchedViewWith(touches: touches_, event: event_) else {
-      return
-    }
-    if toucedView != self.inputView {
-      self.hideKeyboard()
-    }
-  }
-}
-
-protocol HasInputAreaProtocol {
+protocol InputAreaOwnerProtocol {
   var inputArea: CGRect { get }
   var lastY: CGFloat { get set }
 }
 
-extension UIViewController {
+protocol InputAreaAdjusterProtocol {
+  func centerInputAreaWith(_ notification: Notification)
+  func repositionInputArea(_ notification: Notification)
+}
+
+extension ViewControllerWithKeyboard: InputAreaAdjusterProtocol {
   /// 入力エリアをキーボード外のエリアのセンターに位置づける
   /// （Respond to UIKeyboardWillShow or UIKeyboardDidShow）
   func centerInputAreaWith(_ notification: Notification) {
-    guard var viewControllerHasInputArea = self as? HasInputAreaProtocol else {
+    guard var inputAreaOwner = self as? InputAreaOwnerProtocol else {
       return
     }
     guard let keyboardHeight = notification.keyboardHeight() else {
@@ -60,10 +63,10 @@ extension UIViewController {
     let baseRect = CGRect(x: 0, y: 0,
                           width: self.view.frame.width,
                           height: self.view.frame.height - keyboardHeight)
-    let offset = baseRect.midY - viewControllerHasInputArea.inputArea.midY
+    let offset = baseRect.midY - inputAreaOwner.inputArea.midY
 
     var frame = self.view.frame
-    viewControllerHasInputArea.lastY = frame.origin.y
+    inputAreaOwner.lastY = frame.origin.y
     frame.origin.y = offset
     self.view.frame = frame
   }
@@ -71,12 +74,12 @@ extension UIViewController {
   /// 入力エリアを通常の位置に戻す
   /// （Respond to UIKeyboardWillHide or UIKeyboardDidHide）
   func repositionInputArea(_ notification: Notification) {
-    guard var viewControllerHasInputArea = self as? HasInputAreaProtocol else {
+    guard var inputAreaOwner = self as? InputAreaOwnerProtocol else {
       return
     }
 
     var frame = self.view.frame
-    frame.origin.y = viewControllerHasInputArea.lastY
+    frame.origin.y = inputAreaOwner.lastY
     self.view.frame = frame
   }
 }
