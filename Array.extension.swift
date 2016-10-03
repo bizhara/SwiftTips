82236162c90c5b2ffb55b160ceb64c5c66eb3734
Kazuaki Hara <bizhara@gmail.com>
2016-10-03 20:49:59 +0900

    Change method name.

--- Array.extension.swift
+++ Array.extension.swift
@@ -7,13 +7,13 @@
 import Foundation
 
 extension Array where Element: Equatable {
-  mutating func removed(elements elements_: [Element]) {
+  mutating func remove(elements elements_: [Element]) {
     for element in elements_ {
-      self.removed(element: element)
+      self.remove(element: element)
     }
   }
 
-  mutating func removed(element element_: Element) {
+  mutating func remove(element element_: Element) {
     self = filter { $0 != element_ }
   }
 }
