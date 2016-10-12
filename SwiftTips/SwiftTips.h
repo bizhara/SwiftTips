41a220b4daaccb0913bf150067e7d3c3b721a770
Kazuaki Hara <bizhara@gmail.com>
2016-10-12 18:22:00 +0900

    Make framework project.

---
+++ SwiftTips/SwiftTips.h
@@ -0,0 +1,19 @@
+//
+//  SwiftTips.h
+//  SwiftTips
+//
+//  Created by khara on 2016/10/12.
+//  Copyright © 2016年 Kazuaki Hara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftTips.
+FOUNDATION_EXPORT double SwiftTipsVersionNumber;
+
+//! Project version string for SwiftTips.
+FOUNDATION_EXPORT const unsigned char SwiftTipsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftTips/PublicHeader.h>
+
+
