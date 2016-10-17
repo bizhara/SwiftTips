5017b784ecf40bde1bb8a442be76d058c84414f0
Kazuaki Hara <bizhara@gmail.com>
2016-10-17 17:13:17 +0900

    Use typealias.

--- SwiftTips/Codes/ResourceHelpers.swift
+++ SwiftTips/Codes/ResourceHelpers.swift
@@ -8,10 +8,12 @@ import UIKit
 
 /// xib, storyboard の扱い
 public protocol NibProtocol {
+  typealias NibT = Self
+
   static var nibName: String { get }
   static var reuseID: String { get }
-  static func fromNib() -> Self?
-  static func fromStoryboard() -> Self?
+  static func fromNib() -> NibT?
+  static func fromStoryboard() -> NibT?
 }
 
 extension NibProtocol {
@@ -23,17 +25,17 @@ extension NibProtocol {
     return self.nibName
   }
 
-  public static func fromNib() -> Self? {
+  public static func fromNib() -> NibT? {
     let nib = UINib(nibName: self.nibName, bundle: nil)
     let objects = nib.instantiate(withOwner: nil, options: nil)
     guard objects.count > 0 else { return nil }
-    let me = objects[0] as? Self
+    let me = objects[0] as? NibT
     return me
   }
 
-  public static func fromStoryboard() -> Self? {
+  public static func fromStoryboard() -> NibT? {
     let storyboard = UIStoryboard(name: self.nibName, bundle: nil)
-    let me = storyboard.instantiateInitialViewController() as? Self
+    let me = storyboard.instantiateInitialViewController() as? NibT
     return me
   }
 }
