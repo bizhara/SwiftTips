e64b588002edae78e8784304e5ac8feb92589160
bizhara <bizhara@gmail.com>
2017-01-04 21:29:16 +0900

    Delete wrong method.

--- SwiftTips/Codes/UIImage.extension.swift
+++ SwiftTips/Codes/UIImage.extension.swift
@@ -8,28 +8,6 @@ import UIKit
 
 extension UIImage {
   /// View をイメージに変換
-  public class func image(fromViews fromViews_: [UIView])-> UIImage? {
-    func unitedRect(with views_: [UIView])-> CGRect {
-      var unitedRect = CGRect()
-      for view in views_ {
-        if unitedRect.isEmpty {
-          unitedRect = view.frame
-        } else {
-          unitedRect = unitedRect.union(view.frame)
-        }
-      }
-      return unitedRect
-    }
-    let imageRect = unitedRect(with: fromViews_)
-
-    return self.makeImage(with: imageRect.size) { (_ imageContext: CGContext) in
-      for view in fromViews_ {
-        view.layer.render(in: imageContext)
-      }
-    }
-  }
-
-  /// View をイメージに変換
   public class func image(fromView fromView_: UIView)-> UIImage? {
     return self.makeImage(with: fromView_.frame.size) { (_ imageContext: CGContext) in
       fromView_.layer.render(in: imageContext)
