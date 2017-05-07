c344a7f833e3c3ff5e8783bfd0768e5f3ec16024
Kazuaki Hara <bizhara@gmail.com>
2017-05-07 20:48:59 +0900

    Reflect application code as public.

--- SwiftTips/UISearchBar.extension.swift
+++ SwiftTips/UISearchBar.extension.swift
@@ -7,10 +7,14 @@
 import UIKit
 
 extension UISearchBar {
+  private var textFieldKey: String { get { return "_searchField" } }
+  private var cancelButtonKey: String { get { return "_cancelButtonText" } }
+
   /// UITextField の取得
-  open var textField: UITextField? {
-    get {
-      return self.value(forKey: "_searchField") as? UITextField
-    }
+  open var textField: UITextField? { get { return self.value(forKey: self.textFieldKey) as? UITextField } }
+
+  /// キャンセルボタンの表示文字列設定
+  open func setCancelButtonText(with buttonText: String) {
+    self.setValue(buttonText, forKey: self.cancelButtonKey)
   }
 }
