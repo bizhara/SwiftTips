4287156834cad17cb5f9dd70a4c808690d87a2ea
bizhara <bizhara@gmail.com>
2017-05-24 22:33:26 +0900

    Restore initializer.

--- SwiftTips/PlaceholderTextView.swift
+++ SwiftTips/PlaceholderTextView.swift
@@ -60,6 +60,7 @@ open class PlaceholderTextView: UITextView {
 
   required public init?(coder aDecoder: NSCoder) {
     super.init(coder: aDecoder)
+    self.setupPlaceholder()
   }
 
   override open func awakeFromNib() {
