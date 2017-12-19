beb3fe5ffddf5a72e393442f0fb93fe69229c407
bizhara <bizhara@gmail.com>
2017-12-19 23:15:59 +0900

    Add horizonatal.

--- verticalPercentAlignmentValue.playground/Contents.swift
+++ verticalPercentAlignmentValue.playground/Contents.swift
@@ -2,17 +2,29 @@
 
 import UIKit
 
-/// 画面サイズに合わせた相対位置を求める（center verticaly 指定でのパーセント値）
-func verticalPercentAlignmentValue(with superViewFrame_: CGRect, targetViewFrame targetViewFrame_: CGRect) -> CGFloat {
-  let baseHeight = superViewFrame_.size.height / 2
-  let targetHeight = targetViewFrame_.size.height / 2
-  let heightGap = baseHeight - targetHeight
-  let yGap = heightGap - targetViewFrame_.origin.y
-  let targetValue = baseHeight - yGap
-  let percentValue = targetValue / baseHeight
-  return percentValue
+/// 画面サイズに合わせた相対位置を求める（center horizontaly 指定でのパーセント値）
+func horizontalPercentAlignmentValue(withSuperViewFrame: CGRect, targetViewFrame: CGRect) -> CGFloat {
+    let baseWidth = withSuperViewFrame.width / 2
+    let targetHeight = targetViewFrame.width / 2
+    let widthGap = baseWidth - targetHeight
+    let xGap = widthGap - targetViewFrame.origin.x
+    let targetValue = baseWidth - xGap
+    let percentValue = targetValue / baseWidth
+    return percentValue
 }
 
-let superViewFrame = CGRect(x: 0, y: 0, width: 375, height: 667)
-let targetViewFrame = CGRect(x: 25, y: 285, width: 325, height: 333)
-let percentValue = verticalPercentAlignmentValue(with: superViewFrame, targetViewFrame: targetViewFrame)
+/// 画面サイズに合わせた相対位置を求める（center verticaly 指定でのパーセント値）
+func verticalPercentAlignmentValue(withSuperViewFrame: CGRect, targetViewFrame: CGRect) -> CGFloat {
+    let baseHeight = withSuperViewFrame.size.height / 2
+    let targetHeight = targetViewFrame.size.height / 2
+    let heightGap = baseHeight - targetHeight
+    let yGap = heightGap - targetViewFrame.origin.y
+    let targetValue = baseHeight - yGap
+    let percentValue = targetValue / baseHeight
+    return percentValue
+}
+
+let superViewFrame = CGRect(x: 0, y: 0, width: 375, height: 109)
+let targetViewFrame = CGRect(x: 340, y: 54, width: 19, height: 19)
+let horizontalValue = horizontalPercentAlignmentValue(withSuperViewFrame: superViewFrame, targetViewFrame: targetViewFrame)
+let verticalValue = verticalPercentAlignmentValue(withSuperViewFrame: superViewFrame, targetViewFrame: targetViewFrame)
